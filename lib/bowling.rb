b63988942877bd19fbc92046324dc1a7634a5758
tyt-mdk <tm00github@gmail.com>
2024-05-24 14:20:41 +0900

    2.4.3-4 [コードをリファクタリングする]

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -32,11 +32,27 @@ class Bowling
     def calc_score
       @scores.each.with_index(1) do |score, index|
         # 最終フレーム以外でのスペアなら、スコアにボーナスを含めて合計する
-        if score.inject(:+) == 10 && index < 10
-          @total_score += 10 + @scores[index].first
+        if spare?(score) && not_last_frame?(index)
+          @total_score += calc_spare_bonus(index)
         else
           @total_score += score.inject(:+)
         end
       end
+    end
+  
+    private
+    # スペアかどうか判定する
+    def spare?(score)
+      score.inject(:+) == 10
+    end
+  
+    # 最終フレーム以外かどうか判定する
+    def not_last_frame?(index)
+      index < 10
+    end
+  
+    # スペアボーナスを含んだ値でスコアを計算する
+    def calc_spare_bonus(index)
+      10 + @scores[index].first
     end
   end
