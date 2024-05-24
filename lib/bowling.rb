61e2ff0446ed9123ff120a05e184ce78de6d84cd
tyt-mdk <tm00github@gmail.com>
2024-05-24 14:46:06 +0900

    2.4.4-3 [テストが成功するようにプログラムコードを書く]

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -22,23 +22,32 @@ class Bowling
       # 一時保存用のスコアに、倒したピンの数を追加する
       @temp << pins
       # 2投分のデータが入っていれば、1フレーム分のスコアとして全体に追加する
-      if @temp.size == 2
+      if @temp.size == 2 || strike?(@temp)
         @scores << @temp
         @temp = []
       end
     end
   
-    # スコアの合計を計算する
-    def calc_score
-      @scores.each.with_index(1) do |score, index|
-        # 最終フレーム以外でのスペアなら、スコアにボーナスを含めて合計する
-        if spare?(score) && not_last_frame?(index)
-          @total_score += calc_spare_bonus(index)
+  # スコアの合計を計算する
+  def calc_score
+    @scores.each.with_index(1) do |score, index|
+      # 最終フレーム以外でのストライクなら、スコアにボーナスを含めて合計する
+      if strike?(score) && not_last_frame?(index)
+        # 次のフレームもストライクで、なおかつ最終フレーム以外なら、
+        # もう一つ次のフレームの一投目をボーナスの対象にする
+        if strike?(@scores[index]) && not_last_frame?(index + 1)
+          @total_score += 20 + @scores[index + 1].first
         else
-          @total_score += score.inject(:+)
+          @total_score += 10 + @scores[index].inject(:+)
         end
+      # 最終フレーム以外でのスペアなら、スコアにボーナスを含めて合計する
+      elsif spare?(score) && not_last_frame?(index)
+        @total_score += calc_spare_bonus(index)
+      else
+        @total_score += score.inject(:+)
       end
     end
+  end
   
     private
     # スペアかどうか判定する
@@ -54,5 +63,10 @@ class Bowling
     # スペアボーナスを含んだ値でスコアを計算する
     def calc_spare_bonus(index)
       10 + @scores[index].first
+    end
+
+    # ストライクかどうか判定する
+    def strike?(score)
+      score.first == 10
     end
   end
